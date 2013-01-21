7f0beb6ef2f90e81411e5ef03c9fb96a5fda6463
sifeij <sophie.jiang@dial800.com>
2013-01-21 15:29:55 -0800

    Views

--- js/main.js
+++ js/main.js
@@ -5,66 +5,44 @@ var Person = Backbone.Model.extend({
 		name: 'John Doe',
 		age: 30,
 		occupation: 'worker'
-	},
-
-	// this validate method will be only trigged when using set attribute
-	validate: function(attrs) {
-		if( attrs.age < 0 ){
-			return 'Age must be positive, stupid.';
-		}
-
-		if ( !attrs.name ) {
-			return 'Every person must have a name, fool.';
-		}
-	},
-
-	work: function() {
-		return this.get('name') + ' is working.';
 	}
 });
 
-/***************** Test in Console chrome developer tool ***********
+var PersonView = Backbone.View.extend({
+	tagName: 'li', 			//change div tag to li tag
+	className: 'person',	//append class to the tag
+	id: 'some-person',		//append id inside the tag
+
+	// similar to constructor, it will automatically run
+	initialize: function() {
+		//console.log(this.model);
+		this.render();
+	},
+
+	render: function() {
+		this.$el.html( this.model.get('name') 
+			+ ' (' + this.model.get('age') + ') - '  
+			+ this.model.get('occupation') );
+	}
+})
 
 var person = new Person;
-var person = new Person({ name: 'Jeffrey Way', age: 27}); // overwrite the default
+var personView = new PersonView({ model: person });
 
-person;
-person.get('name');
-person.work();
-perosn.toJSON();
-person.set('occupation', 'Web Developer'); // only set one property at a time
-person.set({ occupation: 'instructor'}); // set multiple properties at a time
 
-person.on('error', function(model, error) {
-	console.log(error);
-})
-var person = new Person({ name: 'Jeffrey Way', age: -27}); // test validation
+/***************** Test in Console chrome developer tool ***********
+
+PersonView
+
+var personView = new PersonView;
+
+personView.el    //<div></div>
+personView.$el   //[<div></div>]
+
+personView.render();
+personView.el;
+
+personView.el;
+$(document.body).html(personView.el);
 
 */
-
-//****************** Normal JS ********************
-
-//var Quiz = function(title) {	
-//	this.title = title;
-//};
-
-//// If attache work method to person object like below
-//// everytime when a new person is created, the work method will be executed
-//var Person = function(config) {
-//	this.name = config.name;
-//	this.age = config.age;
-//	this.occupation = config.occupation;
-//	this.work = function() {
-//	}
-//};
-
-//// A better way to define a person with work method
-//var Person = function(config) {
-//	this.name = config.name;
-//	this.age = config.age;
-//	this.occupation = config.occupation;	
-//};
-//
-//Person.prototype.work = function() {
-//	return this.name + ' is working.';
-//};
