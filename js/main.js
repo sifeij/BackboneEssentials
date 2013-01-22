f14ebcc9776999472ea803282211237dd5d850bf
sifeij <sophie.jiang@dial800.com>
2013-01-22 13:40:37 -0800

    change firstName lastName to name

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 // Person Model
 var Person = Backbone.Model.extend({
 	defaults: {
-		firstName: 'John',
-		lastName: 'Doe',
+		name: 'John Doe',
 		age: 30,
 		occupation: 'worker'
-	},
-
-	fullName: function() {
-		return this.get('firstName') + ' ' + this.get('lastName');
 	}
 });
 
@@ -17,35 +12,14 @@ var PeopleCollection = Backbone.Collection.extend({
 	model: Person
 })
 
-//View for all peopel
+//View for all people
 var PeopleView = Backbone.View.extend({
 	tagName: 'ul',
 
-	//initialize: function() {
-	//	console.log(this.collection);
-	//},
-
 	render: function() {
-
-		// filter through all items in a collection
-		// underscore is baked in backbone with methods like _.each  _.forEach 
-		//console.log(this.collection);
-		//console.log(this);
-
-		//this.collection.each( function(model) {
 		this.collection.each( function(person) {
 			var personView = new PersonView({ model: person });
-
-			//console.log(personView);
-			//console.log(personView.el);
-
-			// append to root element
-			//console.log(this);
-			//personView.render();
-			//this.$el.append(personView.el)
-
-			this.$el.append(personView.render().el); // make sure render method has return this to use this code to chain them together
-
+			this.$el.append(personView.render().el);
 		}, this);
 
 		return this;
@@ -60,25 +34,22 @@ var PersonView = Backbone.View.extend({
 
 	render: function() {
 		this.$el.html( this.template(this.model.toJSON()) );
-		return this; // best practice, general rule of thumb, always return this.
+		return this;
 	}
 })
 
 var peopleCollection = new PeopleCollection([
 	{
-		firstName: 'Jeffrey',
-		lastName: 'Way',
+		name: 'Jeffrey Way',
 		age: 27
 	},
 	{
-		firstName: 'John',
-		lastName: 'Doe',
+		name: 'John Doe',
 		age: 50,
 		occupation: 'web designer'
 	},
 	{
-		firstName: 'Sally',
-		lastName: 'Doe',
+		name: 'Sally Doe',
 		age: 29,
 		occupation: 'graphic designer'
 	}
@@ -87,10 +58,3 @@ var peopleCollection = new PeopleCollection([
 var peopleView = new PeopleView({ colloection: peopleCollection });
 
 $(document.body).append(peopleView.render().el);
-
-/***************** Test in Console chrome developer tool ***********
-
-peopleView.render();
-peopleView.el;
-
-*/
